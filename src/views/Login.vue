1b0cc5a91318b7e00f45d1430a2805126d6c8c9d
naopin <naopin708@yahoo.co.jp>
2020-06-11 16:26:00 +0900

    register

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>ログイン画面</h1>
+    <table>
+      <tr>
+        <td>
+          <label for="email">E-mail</label>
+        </td>
+        <td>
+          <input id="email" type="email" v-model="email" />
+        </td>
+      </tr>
+
+      <tr>
+        <td>
+          <label for="password">Password</label>
+        </td>
+        <td>
+          <input id="password" type="password" v-model="password" />
+        </td>
+      </tr>
+    </table>
+    <br>
+    <br>
+
+    <button @click="register">新規登録</button>
+    <br>
+    <router-link to="/register">新規登録はこちらから</router-link>
+  </div>
+</template>
